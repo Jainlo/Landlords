05148be633693900670af2cd2b8d454e9c3e52c8
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2022-10-24 13:14:49 +0300

    Inital data exploration

--- aqar.py
+++ aqar.py
@@ -7,5 +7,26 @@ df = pd.read_csv('SA_Aqar.csv')
 df.info()
 
 #%%
+df
+
+#%% Check for missing values
+df.isnull().sum()
+
+#%% Drop details column
+df.drop('details', axis=1, inplace=True)
+
+#%% Check for duplicates
+df[~df.duplicated()]
+
+#%% Mean per city
+
+#%%
 df.sample(5)
-# %%
+
+#%% The majority of type of units in each city
+
+#%% Average price per district
+
+#%% Relationship between Price and features
+
+#%% What type of rental units have the highest rent value.
