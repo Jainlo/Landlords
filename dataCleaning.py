001459614f293ac12ae15bb228616d1dddf5a0c7
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2022-10-26 19:04:03 +0300

    removed unnessercy return type

--- dataCleaning.py
+++ dataCleaning.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 #%% Reading the data
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+def clean(df: pd.DataFrame):
 
 
     # 2. Details column has missing data and is made up of unstructured data
